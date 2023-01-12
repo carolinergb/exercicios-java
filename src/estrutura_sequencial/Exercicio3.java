56c5c293cf7e874e6cb3a674bfc0d79e7edfe3bc
Caroline <114437360+carolinergb@users.noreply.github.com>
2023-01-11 22:16:02 -0300

    adicionado exercício 3

---
+++ src/estrutura_sequencial/Exercicio3.java
@@ -0,0 +1,25 @@
+package estrutura_sequencial;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c, d, diferenca;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		d = sc.nextInt();
+		diferenca = a * b - c * d;
+		
+		System.out.println("Diferença = " + diferenca);
+		
+		sc.close();
+
+	}
+
+}
